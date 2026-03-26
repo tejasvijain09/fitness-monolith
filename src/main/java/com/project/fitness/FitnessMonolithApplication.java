e3a11ba88815e83b4b3abab3f2f4d0e7782ce7fd
Tejasvi <tejasvijain09@gmail.com>
2026-03-26 10:54:10 +0530

    created entities and their relationships

---
+++ src/main/java/com/project/fitness/FitnessMonolithApplication.java
@@ -0,0 +1,14 @@
+package com.project.fitness;
+
+import com.project.fitness.model.Activity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessMonolithApplication.class, args);
+	}
+
+}
